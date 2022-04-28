f35cd83a4cbf66b4410f0f5f9c75a48265225a56
kodbazis <mate@kodbazis.hu>
2022-04-28 11:10:04 +0200

    app init

---
+++ angular-pelda-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // angular devtools
+  // State
+  xPozicio = 90;
+  yPozicio = 200;
+  mozgatvaVan = true;
+
+
+
+}
--- drag-and-drop-sablon/script.js
+++ drag-and-drop-sablon/script.js
@@ -1,52 +1,88 @@
+// A. Komponens
 
-// Állapot
-let state = {
-  xPozicio: 90, 
-  yPozicio: 200,
-  mozgatvaVan: false
+kivalasztottSzin = "blue";
+
+function renderA() {
+  document.getElementById("szinvalaszto").innerHTML = `
+  <form onsubmit="formSubmitTortent(window.event)">
+          <select name="szin">
+            <option value="blue">Kék</option>
+            <option value="red">Piros</option>
+            <option value="green">Zöld</option>
+          </select>
+          <button type="submit">Küldés</button>
+    </form>
+
+    <div style="width: 100px; height: 100px; background-color: ${kivalasztottSzin}"></div>
+  `
 }
+
+function formSubmitTortent(e) {
+  e.preventDefault();
+  kivalasztottSzin = e.target.elements.szin.value;
+  renderA();
+}
+
+window.addEventListener('load', renderA)
+
+
+
+
+// B. Komponens -------------------------------------------------------
+
+// Kontroller
+  // Állapot
+  let state = {
+    xPozicio: 90, 
+    yPozicio: 200,
+    mozgatvaVan: false
+  }
+
+  // action
+  function mouseDownEsemenyTortent() {
+
+    // state change
+    state.mozgatvaVan = true;
+    console.log(state);
+
+    // render
+    render();
+  }
+
+  function mouseUpEsemenyTortent() {
+    state.mozgatvaVan = false;
+    console.log(state);
+    render();
+  }
+
+  function mouseMoveTortent(event) {
+    if(state.mozgatvaVan) {
+      state.xPozicio = event.clientX - 150;
+      state.yPozicio = event.clientY - 75;
+    }
+    render();
+  }
 
 // initial render
-window.onload = render;
+window.addEventListener('load', render)
 
-function render() { 
-    document.getElementById("drag-and-drop-app").innerHTML = `
-      <div 
-        class="box ${state.mozgatvaVan ? "grabbed" : "not-grabbed"}" 
-        style="width: 300px; position: absolute; top: ${state.yPozicio}px; left: ${state.xPozicio}px"
-        onmousedown="mouseDownEsemenyTortent()"
-        onmouseup="mouseUpEsemenyTortent()"
-        onmousemove="mouseMoveTortent(window.event)"
-        >
-        <div class="card-body">
-          <h5 class="card-title display-6"># Mozgatható doboz</h5>
+
+
+// Sablon - template ------------------------
+  function render() { 
+      document.getElementById("drag-and-drop-app").innerHTML = `
+        <div 
+          class="box ${state.mozgatvaVan ? "grabbed" : "not-grabbed"}" 
+          style="width: 300px; position: absolute; top: ${state.yPozicio}px; left: ${state.xPozicio}px; background: ${kivalasztottSzin}"
+          onmousedown="mouseDownEsemenyTortent()"
+          onmouseup="mouseUpEsemenyTortent()"
+          onmousemove="mouseMoveTortent(window.event)"
+          >
+          <div class="card-body">
+            <h5 class="card-title display-6"># Mozgatható doboz</h5>
+          </div>
         </div>
-      </div>
-    `;
-}
-
-
-// action
-function mouseDownEsemenyTortent() {
-
-  // state change
-  state.mozgatvaVan = true;
-  console.log(state);
-
-  // render
-  render();
-}
-
-function mouseUpEsemenyTortent() {
-  state.mozgatvaVan = false;
-  console.log(state);
-  render();
-}
-
-function mouseMoveTortent(event) {
-  if(state.mozgatvaVan) {
-    state.xPozicio = event.clientX - 150;
-    state.yPozicio = event.clientY - 75;
+      `;
   }
-  render();
-}
+
+
