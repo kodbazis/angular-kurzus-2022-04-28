3891cd105cbf5b7cda4c043eb26bdf349c0ae086
kodbazis <mate@kodbazis.hu>
2022-04-28 12:47:19 +0200

    init

--- angular-pelda-app/src/app/app.component.ts
+++ angular-pelda-app/src/app/app.component.ts
@@ -13,4 +13,20 @@ export class AppComponent {
     this.kivalasztottSzin = szin;
   }
 
+  elemek = [
+    "Teszt szöveg 1.",
+    "Teszt szöveg 2.",
+    "Teszt szöveg 3.",
+  ];
+
+  elemTorolve(i: number) {
+      console.log(i);
+      this.elemek.splice(i, 1);
+  }
+
+  ujElemLetrehozva(event: any) {
+    event.preventDefault();
+    this.elemek.push(event.target.elements.elem.value);
+    event.target.reset();
+  }
 }
